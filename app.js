1ec0ba63ed9adeb06c356d73af234869fa53495a
rebeb74 <bertrandpetit10@gmail.com>
2021-08-21 10:54:44 +0200

    whitelist

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const {
 const helmet = require('helmet');
 require('dotenv').config();
 
-var whitelist = ['https://www.codeattila.ch', 'https://www.v1.codeattila.ch', 'https://www.v2.codeattila.ch', 'http://localhost:4200', 'http://192.168.1.117:4200'];
+var whitelist = ['https://www.codeattila.ch', 'https://www.v1.codeattila.ch', 'https://www.v2.codeattila.ch', 'http://localhost:4200', 'http://192.168.1.117:4200', 'https://codeattila.ch'];
 // Instantiate server
 const app = express();
 const http = require('http');
